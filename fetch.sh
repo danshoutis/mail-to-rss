e9747bdc299dfe3ef2ea822afd9d3fcade17a4b9
Dan Shoutis <dan@ms-wormwood.(none)>
2010-04-30 13:12:02 -0600

    Added url root + working links in the rss feed!

--- fetch.sh
+++ fetch.sh
@@ -3,13 +3,12 @@ SCRIPT=$(readlink -f $0)
 ROOT=`dirname $SCRIPT`
 
 # read config vars
-. $ROOT/config.user
 . $ROOT/config.system
-
+. $ROOT/config.user
 
 
 # Add mhonarc to path:
-PERL5LIB=$MHONARC/lib $PERL5LIB
+PERL5LIB=$MHONARC/lib:$PERL5LIB
 export PERL5LIB
 
 # Impl function: Run fetchmail w/ stdin config
@@ -18,14 +17,15 @@ go()
     LABEL=$1
     # Make sure HTML folder exists.
     mkdir -p $HTMLPATH/$LABEL
-
+    export LABEL
+    export URLROOT
     fetchmail --fetchmailrc - <<EOF
-poll $SERVER protocol IMAP
+poll $SERVER protocol $PROTO
   user $ADDR with password "$SECRET"
   folder $LABEL
-  ssl
+  $SSL
   keep
-mda "perl $MHONARC/examples/mha-preview -main -folrefs -outdir $HTMLPATH/$LABEL -otherindexes $ROOT/mhonarc-rss.rc --add"
+mda "perl $MHONARC/examples/mha-preview -prv-maxlen 2048 -main -folrefs -outdir $HTMLPATH/$LABEL -otherindexes $ROOT/mhonarc-rss.rc --add"
 
 EOF
 }
